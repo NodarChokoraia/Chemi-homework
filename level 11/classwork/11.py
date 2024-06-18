93a94bd5889efe5634ed99c396fcc61bb4a51a16
NodarChokoraia <nodargamer1@gmail.com>
2024-06-18 16:52:44 +0400

    second commit

---
+++ level 11/classwork/11.py
@@ -0,0 +1,9 @@
+name = input("what is your name:")
+name1 = input("what is your name:")
+print(name)
+print(name1)
+
+name2 = input("Mark:")
+name3 = input("Jonh:")
+print(name2 + ",Mark")
+print(name3 + ",Jonh")
