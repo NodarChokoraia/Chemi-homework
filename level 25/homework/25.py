161e17dc0831ef6daefd6e620c70fdde9567e94a
NodarChokoraia <nodargamer1@gmail.com>
2024-08-11 12:42:57 +0400

    second commit

---
+++ level 25/homework/25.py
@@ -0,0 +1,153 @@
+for i in range(10):
+    print(i)
+
+
+
+
+for i in range(10, 1):
+    print(i)
+
+
+
+for i in range(10, 1, 5):
+    print(i)
+
+
+
+
+for i in range(10, 10 , -1):
+    print(i)
+
+
+
+
+for i in range(100,100 , -6    ):
+    print(i)
+
+
+
+
+for i in range(106, 2 , 2):
+    print(i)
+
+
+
+
+
+
+for i in range(101, 101, -1):
+    print(i)
+
+
+for i in range(1000, 1000, -100):
+    print(i)
+
+
+
+for i in range(10, 1 ):
+    print(i)
+
+
+for i in range(10, 2):
+    print(i)
+
+
+for i in range(10, 10 , 1):
+    print(i)
+
+
+
+nodiko = int(input("number:"))
+nodiko1 = int(input("guest number:"))
+
+
+
+
+
+
+while nodiko != nodiko1:
+    nodiko1 = int(input("guest number:"))
+
+
+
+nodiko2 = int(input("number:"))
+nodiko3 = int(input("number 2:"))
+
+
+
+while nodiko2 <= nodiko3:
+    nodiko3 = int(input("number 2:"))
+
+
+nodiko4 = int(input("number:"))
+nodiko5 = int(input("number 2:"))
+
+
+
+while nodiko4 >= nodiko5:
+    nodiko5 = int(input("number 2:"))
+
+
+
+
+
+
+nodiko6 = int(input("number:"))
+nodiko7 = int(input("number 2:"))
+
+
+
+while nodiko6 == nodiko7:
+    nodiko7 = int(input("number 2:"))
+
+
+
+
+nodiko8 = int(input("number:"))
+nodiko9 = int(input("number 2:"))
+
+
+
+while nodiko8 != nodiko9:
+    nodiko9 = int(input("number 2:"))
+
+
+nodiko10 = int(input("number:"))
+nodiko11 = int(input("number 2:"))
+
+
+
+while nodiko10 < nodiko11:
+    nodiko11 = int(input("number 2:"))
+
+nodiko12 = int(input("number:"))
+nodiko13 = int(input("number 2:"))
+
+
+
+while nodiko12 > nodiko13:
+    nodiko13 = int(input("number 2:"))
+
+nodiko14 = int(input("number:"))
+nodiko15 = int(input("number 2:"))
+
+
+
+while nodiko14 == nodiko15:
+    nodiko15 = int(input("number 2:"))
+
+nodiko16 = int(input("number:"))
+nodiko17 = int(input("number 2:"))
+
+
+
+while nodiko16 != nodiko17:
+    nodiko17 = int(input("number 2:"))
+
+nodiko18 = int(input("number:"))
+nodiko19 = int(input("number 2:"))
+
+
+
+while nodiko18 < nodiko19:
+    nodiko19 = int(input("number 2:"))
