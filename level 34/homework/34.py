7f14a1eeaccb15d4ae5b7639c21da16ff6105d29
NodarChokoraia <nodargamer1@gmail.com>
2024-09-08 17:18:31 +0400

    second commit

---
+++ level 34/homework/34.py
@@ -0,0 +1,29 @@
+
+num1 = int(input('first number:'))
+num2 = int(input('secend number:'))
+
+calcurator = input('and calcurator:')
+
+
+if calcurator == ('+'):
+    print(num1 + num2)
+
+
+
+
+elif calcurator == ('-'):
+    print(num1 - num2)
+
+elif calcurator == ('*'):
+    print(num1 * num2)
+    
+
+elif calcurator == ('/'):
+    print(num1 / num2)
+
+
+
+
+
+
+
