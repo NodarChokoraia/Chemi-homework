aa2e1d0b46de560ee35a160e3561e8775523f40d
NodarChokoraia <nodargamer1@gmail.com>
2025-02-10 02:25:30 +0400

    second commit

---
+++ level 80/classwork/script.js
@@ -0,0 +1,45 @@
+let i = 0;
+while(i<50) {
+    console.log('GOA');
+    i++;
+    
+}
+
+
+
+let n = 20;
+while(n>=0) {
+    console.log(n);
+    n--;
+    
+}
+
+
+let m = 1;
+while(m<20) {
+    m++;
+    if(m%2==0){
+        console.log(m);
+        
+    }else{
+        console.log('-----------------');
+    }
+
+    
+    
+    
+}
+
+
+
+let t = 0;
+while(t<100) {
+    
+    t++;
+    if(t%5==0){
+      console.log(t);  
+    }else{
+        console.log('-----------------');
+    }
+    
+}
